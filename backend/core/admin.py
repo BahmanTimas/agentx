4bfd5316c99461a1c3e6cb67ae6a6a9b5e8ff142
Amin Rostami <amin.rostami@e.mail>
2024-10-29 17:14:07 +0330

    feat: add configuration to admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from backend.core.models import PostDetail, Conversation
+from backend.core.models import PostDetail, Conversation, Configuration
 
 admin.site.register(PostDetail)
 admin.site.register(Conversation)
+admin.site.register(Configuration)
